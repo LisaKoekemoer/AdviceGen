9874e1e42c5b03688a004900f432a8d97adb3635
Lisa Koekemoer <alicia.koekemoer98@gmail.com>
2024-11-20 09:41:36 +0200

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import desktop from './images/pattern-divider-desktop.svg';
+import mobile from './images/pattern-divider-mobile.svg';
+import dice from './images/icon-dice.svg';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [text, setText] = useState([])
+
+  const fetchAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    console.log(data)
+
+    setText(data.slip)
+  }
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <h3>Advice #{text.id}</h3>
+        <p>{text.advice}</p>
+        <picture>
+          <source media="(min-width: 768px)" srcSet={desktop} />
+          <img src={mobile} alt="" />
+        </picture>   
+        <button onClick={fetchAdvice}>
+          <img src={dice} alt="" />
+        </button> 
+      </div>
     </div>
   );
 }
